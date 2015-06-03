87381075a34757ce0b4e0782c52dfc00f2f42308
theflash956 <kick123flip@hotmail.com>
2015-06-03 11:20:01 -0400

    Create Chat.cs

---
+++ Chat.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using SharpDX;
+using LeagueSharp.Common;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomEvents.Game OnGameLoad;
+
+            Game.Say("/all Do it the way i like....");
+        }
+
+        static void Main(Game.End) AssemblyLoadEventArgs;
+
+        CustomEvents.Game OnGameEnd;
+
+            Game.Say("/all gg ez, report taric")
+    }
+}
